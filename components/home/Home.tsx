90e2911dc47ef9a39539cf5a6d40b736c8e32ebf
zqusgurz <rexs333@naver.com>
2023-05-11 14:20:46 +0900

    color

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -6,9 +6,27 @@ import { data } from 'autoprefixer';
 import { app } from 'firebase-admin';
 import { db } from '../../firebaseConfig';
 import { motion } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 
+const transition = {
+    duration: 1.5,
+    ease: [0.6, -0.05, 0.01, 0.9],
+  }
+  
+  const textReveal = {
+    initial: {
+      x: "100%",
+      opacity: 0,
+    },
+    animate: {
+      x: "0%",
+      opacity: 1,
+    },
+  };
 
 const Home = () => {
+    const isMobile = useMediaQuery({ query: "(max-width : 767px)" }); // 모바일 화면일 때 조건을 추가해줍니다.
+
     const [phoneNumber, setPhoneNumber] = useState('');
     const [demerit, setDemerit] = useState<number | null>(null);
     const [demerit1, setDemerit1] = useState<number | null>(null);
@@ -56,30 +74,30 @@ const Home = () => {
 
     return (
         <section id="home" className="container flex flex-col items-center justify-center h-screen">
-            <div className="font-bold " style={{ fontSize: 48, marginBottom: '3vw' }}>미팅놈들 벌점 조회</div>
+            <div className="font-bold " style={{ fontSize: 48, marginBottom: '3vw', color: isMobile ? '#FFF' : '#000' }}>미팅놈들 벌점 조회</div>
             <form onSubmit={handleFormSubmit}>
                 <div className="form-control">
-                    <p style={{ fontSize: 18, marginBottom: '1vw', color: '#111', textAlign: 'center' }}>아래 입력창에 01012341234와 같은 형식으로 번호 입력 후 조회 버튼을 눌러주세요</p>
-                    <label htmlFor="number" style={{ fontSize: 24, marginRight: '2vw', color: '#333' }}>전화번호 : </label>
+                    <p style={{ fontSize: 18, marginBottom: '1vw', color: isMobile ? '#FFF' : '#000', textAlign: 'center' }}>아래 입력창에 01012341234와 같은 형식으로 번호 입력 후 조회 버튼을 눌러주세요</p>
+                    <label htmlFor="number" style={{ fontSize: 24, marginRight: '2vw', color: isMobile ? '#FFF' : '#000' }}>전화번호 : </label>
                     <input type="text" value={phoneNumber} onChange={handlePhoneNumberChange} id="number" style={{ fontSize: 24, border: '2px solid #ccc' }} />
                 </div>
 
                 <div className="form-actions" style={{ textAlign: 'center', marginTop: '2vw' }}>
-                    <button style={{ fontSize: 20, border: '2px solid #ddd', padding: '0.2rem 2rem', backgroundColor: 'eee', color: '#333' }}>조회</button>
+                    <button style={{ fontSize: 20, border: '2px solid #ddd', padding: '0.2rem 2rem', backgroundColor: 'eee', color: isMobile ? '#FFF' : '#000' }}>조회</button>
                 </div>
             </form>
             {demerit !== null && (
-                <div style={{ textAlign: 'center', marginTop: '2vw', fontSize: 30, color: '#333' }}>
+                <div style={{ textAlign: 'center', marginTop: '2vw', fontSize: 30, color: isMobile ? '#FFF' : '#000' }}>
                     상벌점 : {demerit}
                 </div>
             )}
             {demerit1 !== null && (
-                <div style={{ textAlign: 'center', marginTop: '2vw', fontSize: 30, color: '#333' }}>
+                <div style={{ textAlign: 'center', marginTop: '2vw', fontSize: 30, color: isMobile ? '#FFF' : '#000' }}>
                     기타 벌점 : {demerit1}
                 </div>
             )}
             {demerit2 !== null && (
-                <div style={{ textAlign: 'center', marginTop: '2vw', fontSize: 30, color: '#333' }}>
+                <div style={{ textAlign: 'center', marginTop: '2vw', fontSize: 30, color: isMobile ? '#FFF' : '#000' }}>
                     파토 벌점 : {demerit2}
                 </div>
             )}
